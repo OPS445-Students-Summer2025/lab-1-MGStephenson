7f89ac17d545ad91eb77a2cbfae9c920b2203c6e
Mario Stephenson <marketingmario19@gmail.com>
2025-05-19 19:03:38 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''This script performs math using variables and prints the result.'''
+
+x = 10
+y = 2
+z = 5
+
+result = x + y * z
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(result))
