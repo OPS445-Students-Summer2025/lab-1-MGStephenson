361d7b89e19d058bcc191f035d1cab197b720fe0
Mario Stephenson <marketingmario19@gmail.com>
2025-05-19 18:56:10 -0400

    Added lab1a

---
+++ lab1a.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+'''Description: This program will output "hello world" to the screen.'''
+
+# Any line that starts with a "#" is a comment
+# The line below prints text to the screen
+print('Hello world')
